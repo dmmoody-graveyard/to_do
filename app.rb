279cf0d15883c3850cf4ee8d09e2d9ffd003b5c2
Duane M Moody <dmmoody@gmail.com>
2015-01-26 12:34:32 -0800

    trouble shoot

--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
 require("sinatra")
 require("sinatra/reloader")
-require('sinatra/activerecord')
 also_reload("lib/**/*.rb")
-require("./lib/task")
 require("pg")
+require("sinatra/activerecord")
+require("./lib/task")
 
 
 get('/') do
